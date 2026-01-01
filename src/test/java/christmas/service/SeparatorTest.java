ec5e0791380a4d88b03d6065bd234986e90f0a8e
K-iise <91732817+K-iise@users.noreply.github.com>
2026-01-01 20:36:05 +0900

    feat: 메뉴 이름, 개수 분리 테스트 추가

--- src/test/java/christmas/service/SeparatorTest.java
+++ src/test/java/christmas/service/SeparatorTest.java
@@ -1,13 +1,36 @@
 package christmas.service;
 
 import java.util.HashMap;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SeparatorTest {
 
     @Test
-    public void 메뉴_분리_테스트(){
+    public void 메뉴_형식_분리_테스트(){
+        Separator separator = new Separator();
+        String data = "해산물파스타-1,레드와인-2,초코케이크-1";
+        String[] menu = separator.splitByComma(data);
+        
+        Assertions.assertThat(menu.length).isEqualTo(3);
+    }
+
+    @Test
+    public void 메뉴_이름_분리_테스트(){
+        Separator separator = new Separator();
+        String data = "해산물파스타-2";
+        String name = separator.splitByName();
+
+        Assertions.assertThat(name).isEqualTo("해산물파스타");
+    }
+
+    @Test
+    public void 메뉴_개수_분리_테스트(){
+        Separator separator = new Separator();
+        String data = "해산물파스타-2";
+        int quantity = separator.splitByQuantity(data);
+
+        Assertions.assertThat(quantity).isEqualTo(2);
     }
 
 }
